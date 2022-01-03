cc80b74e90a8487d2214f57b71e8a2762ef281a4
test <test@gmail.com>
2022-01-03 16:55:40 +0530

    changes

---
+++ server.js
@@ -0,0 +1,24 @@
+express = require('express');
+
+let app = express();
+const bodyParser = require('body-parser');
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function(req,res){
+	res.send("response done");
+})
+
+
+var server = app.listen( process.env.PORT || 2000, function(){
+    console.log('Listening on port ' + server.address().port);
+});
+
+module.exports = app;
